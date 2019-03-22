1bf50c90fcb043dab6d2da7d603f48dd158b0991
dev5151 <devaansh51@gmail.com>
2019-03-22 21:20:00 +0530

    First Commit

---
+++ AndroidStudioProjects/EKISAN/app/src/main/java/com/example/android/ekisan/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.ekisan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+
+        TextView numbers = (TextView) findViewById(R.id.weather);
+    }
+}
+
+
---
+++ AndroidStudioProjects/EKISAN/app/src/main/java/com/example/android/ekisan/WeatherActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.ekisan;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+
+    }
+
+    Intent i =
+
+}
